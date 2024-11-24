0951a440dd6a6b6edca7602ac07b0af8e8d8faa3
kozlov-ae <allek-007@list.ru>
2024-11-24 06:54:26 +0300

    documentation

---
+++ src/types/index.ts
@@ -0,0 +1,55 @@
+//----------------------------- Типы, отвечающие за хранение данных и работу с Api ------------------------------
+export type TPaymentType = 'online ' | 'cash';
+
+export type TProductCategory =
+	| 'софт-скил'
+	| 'кнопка'
+	| 'хард-скил'
+	| 'дополнительное'
+	| 'другое';
+
+export interface IProduct {
+	id: string;
+	title: string;
+	description: string;
+	image: string;
+	category: TProductCategory;
+	price: number;
+}
+
+export interface IOrdering {
+	email: string;
+	phone: string;
+	paymentType: TPaymentType;
+	address: string;
+	total: number;
+	items: string[];
+
+	Send(): void;
+}
+
+export interface IProductsData {
+	products: IProduct[];
+	selectedProduct: IProduct | null;
+
+	addProduct(product: IProduct): void;
+	getProduct(id: string): IProduct;
+	updateProduct(id: string, newProduct: IProduct): void;
+	deleteProduct(id: string): void;
+}
+
+export interface IOrderingData {
+	setOrderDetails(details: TOrderDetails): IOrdering;
+	setClientDetails(details: TClientDetails): IOrdering;
+	addProduct(productId: string): void;
+	deleteProduct(productId: string): void;
+	getOrdering(): IOrdering;
+	toOrder(): boolean;
+	checkOrdering(): boolean;
+	clear(): void;
+}
+
+// ---------- UI types ----------
+export type TProductCardData = Pick<IProduct, 'id' | 'title' | 'image' | 'description' | 'category' | 'price'>;
+export type TOrderDetails = Pick<IOrdering, 'paymentType' | 'address'>;
+export type TClientDetails = Pick<IOrdering, 'email' | 'phone'>;
